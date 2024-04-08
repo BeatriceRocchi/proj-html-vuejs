26d658f6b224031d157d61c7f0a4320f9aab6b2a
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-04-08 15:45:37 +0200

    [add] header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,23 @@
 <script>
-export default {};
+import Navbar from "./partials/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
 </script>
 
 <template>
-  <header>H</header>
+  <header class="container container_custom d-flex justify-content-between">
+    <img class="logo" src="../assets/img/logo.png" alt="Logo Fable" />
+    <Navbar />
+  </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  .logo {
+    width: 185px;
+  }
+}
+</style>
--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,9 +1,57 @@
 <script>
-export default {};
+import navItems from "../../assets/data/db_navbar";
+export default {
+  data() {
+    return {
+      navItems,
+      isHover: false,
+    };
+  },
+  methods: {
+    getImagePath(img) {
+      return new URL(`../../assets/img/${img}`, import.meta.url).href;
+    },
+  },
+};
 </script>
 
 <template>
-  <div></div>
+  <ul class="d-flex align-items-center">
+    <!-- TODO: fix icons on hover effects -->
+    <li
+      class="d-flex flex-column align-items-center justify-content-center"
+      v-for="item in navItems"
+      :key="item.id"
+      @mouseenter="isHover = true"
+      @mouseleave="isHover = false"
+    >
+      <div class="img_box">
+        <img v-if="!isHover" :src="getImagePath(item.icon)" :alt="item.title" />
+        <img v-else :src="getImagePath(item.icon2)" :alt="item.title" />
+      </div>
+      <a href="#">{{ item.title }}</a>
+    </li>
+  </ul>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../../assets/scss/partials/variables";
+
+li {
+  padding: 0 28px;
+  width: 98px;
+  height: 100%;
+  color: $color-secondary;
+
+  &:hover {
+    cursor: pointer;
+    background-color: $color-primary;
+    color: $color-white;
+  }
+
+  .img_box {
+    height: 32px;
+    margin-bottom: 15px;
+  }
+}
+</style>
