3d632d009a23a651bcaa8c5bf3d65d36387531d5
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-04-10 18:38:14 +0200

    [add] hover effects on gallery images

--- src/components/partials/Gallery.vue
+++ src/components/partials/Gallery.vue
@@ -4,27 +4,88 @@ export default {};
 
 <template>
   <div class="gallery_wrapper d-flex">
-    <img
-      src="../../assets/img/img_gallery/gallery_01.jpg"
-      alt="Child drawing"
-    />
-    <img
-      src="../../assets/img/img_gallery/gallery_02.jpg"
-      alt="Children painting"
-    />
-    <img
-      src="../../assets/img/img_gallery/gallery_03.jpg"
-      alt="Child colouring"
-    />
-    <img
-      src="../../assets/img/img_gallery/gallery_04.jpg"
-      alt="Child in a park"
-    />
+    <div class="img_box">
+      <img
+        src="../../assets/img/img_gallery/gallery_01.jpg"
+        alt="Child drawing"
+      />
+      <img
+        class="hover_img"
+        src="../../assets/img/image_overlay.png"
+        alt="Plus icon"
+      />
+    </div>
+
+    <div class="img_box">
+      <img
+        src="../../assets/img/img_gallery/gallery_02.jpg"
+        alt="Child painting"
+      />
+      <img
+        class="hover_img"
+        src="../../assets/img/image_overlay.png"
+        alt="Plus icon"
+      />
+    </div>
+
+    <div class="img_box">
+      <img
+        src="../../assets/img/img_gallery/gallery_03.jpg"
+        alt="Child colouring"
+      />
+      <img
+        class="hover_img"
+        src="../../assets/img/image_overlay.png"
+        alt="Plus icon"
+      />
+    </div>
+
+    <div class="img_box">
+      <img
+        src="../../assets/img/img_gallery/gallery_04.jpg"
+        alt="Child in a park"
+      />
+      <img
+        class="hover_img"
+        src="../../assets/img/image_overlay.png"
+        alt="Plus icon"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.gallery_wrapper img {
-  width: 25%;
+@import "../../assets/scss/partials/variables";
+
+.gallery_wrapper {
+  height: 350px;
+
+  .img_box {
+    width: 25%;
+    position: relative;
+
+    &:hover .hover_img {
+      display: block;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .hover_img {
+      display: none;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      background-color: rgba($color-secondary, 50%);
+      padding: 25px;
+      height: 80px;
+      width: 80px;
+      border-radius: 50%;
+    }
+  }
 }
 </style>
